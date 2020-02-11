88c1d41dccff69ab2e086a77970b7e87bdf09881
Simone Turco <simone.turco@charite.de>
2020-02-11 19:38:21 +0100

    correction of the Reject button

--- src/ApprovalCard.js
+++ src/ApprovalCard.js
@@ -7,7 +7,7 @@ const ApprovalCard = () => {
       <div className="extra content">
         <div className="ui two buttons">
           <div className="ui basic green button">Approve</div>
-          <div className="ui basic green button">Reject</div>
+          <div className="ui basic red button">Reject</div>
         </div>
       </div>
     </div>
