2d41bc3f406ca3b48e9033a323f996a7c4c09185
Simone Turco <simone.turco@charite.de>
2020-02-11 19:35:01 +0100

    structure of ApprovalCard

--- src/ApprovalCard.js
+++ src/ApprovalCard.js
@@ -1,9 +1,17 @@
-import React from 'react';
+import React from "react";
 
 const ApprovalCard = () => {
-    return(
-
-    );
+  return (
+    <div className="ui card">
+      <div className="content">Are you sure?</div>
+      <div className="extra content">
+        <div className="ui two buttons">
+          <div className="ui basic green button">Approve</div>
+          <div className="ui basic green button">Reject</div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ApprovalCard;
