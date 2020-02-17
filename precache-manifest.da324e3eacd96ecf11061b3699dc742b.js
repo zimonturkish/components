8de5a9850131718740abcd959141b35914dcd0ae
Turco <turcos@charite.de>
2020-02-17 09:48:49 +0100

    Updates

---
+++ precache-manifest.da324e3eacd96ecf11061b3699dc742b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad17ade92e4ef55b13ca8b757feb2062",
+    "url": "/components/index.html"
+  },
+  {
+    "revision": "ec78251ca253c8df2809",
+    "url": "/components/static/js/2.f80285ce.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/components/static/js/2.f80285ce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2cfd76748662428bc1bf",
+    "url": "/components/static/js/main.b2600756.chunk.js"
+  },
+  {
+    "revision": "d0070c91204bf59aeaf0",
+    "url": "/components/static/js/runtime-main.5fe9a117.js"
+  }
+]);
